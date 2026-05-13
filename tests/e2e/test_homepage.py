d63b0708aaff715819cc9ce30008f85dc4e9998e
Ryan Murray <ryan.murray@madetech.com>
2026-05-13 17:40:07 +0100

    refactor(test): use open_homepage fixture in duty description input has label test

--- tests/e2e/test_homepage.py
+++ tests/e2e/test_homepage.py
@@ -19,10 +19,8 @@ def test_duty_number_input_has_label(open_homepage):
 def test_duty_description_exists(open_homepage):
     expect(open_homepage.locator("#duty-description-input")).to_be_visible()
 
-def test_duty_description_input_has_label(page):
-    page.goto("http://localhost:5000")
-
-    expect(page.get_by_label("Duty Description")).to_be_visible()
+def test_duty_description_input_has_label(open_homepage):
+    expect(open_homepage.get_by_label("Duty Description")).to_be_visible()
 
 def test_add_duty_button_exists(open_homepage):
     expect(open_homepage.get_by_role("button", name="Add Duty")).to_be_visible()
