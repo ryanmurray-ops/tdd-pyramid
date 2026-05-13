aa12ab39f6e208430c71b5e85f557a4f76becb2c
Ryan Murray <ryan.murray@madetech.com>
2026-05-13 17:36:11 +0100

    refactor(test): use open_homepage fixture in duties section exists

--- tests/e2e/test_homepage.py
+++ tests/e2e/test_homepage.py
@@ -7,10 +7,8 @@ def test_homepage_has_automate_coin_title(open_homepage):
 def test_homepage_has_h1(open_homepage):
     expect(open_homepage.locator("h1")).to_contain_text("Automate Coin")
 
-def test_duties_section_exists(page):
-    page.goto("http://localhost:5000")
-
-    expect(page.locator("#duties-section")).to_be_visible()
+def test_duties_section_exists(open_homepage):
+    expect(open_homepage.locator("#duties-section")).to_be_visible()
 
 def test_duty_input_exists(page):
     page.goto("http://localhost:5000")
