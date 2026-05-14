19a8e1b47f1635f21d875aad3ca5b4f17fe67bf9
Ryan Murray <ryan.murray@madetech.com>
2026-05-14 09:34:29 +0100

    test(e2e): test to prevent dupicate duty nymber from being added

--- tests/e2e/test_add_duty.py
+++ tests/e2e/test_add_duty.py
@@ -7,3 +7,18 @@ def test_user_can_add_a_duty(open_homepage):
     page.click("text=Add Duty")
 
     expect(page.locator("#duties-list")).to_contain_text("1 - My First Duty")
+
+def test_user_cannot_add_duplicate_duty_number(open_homepage):
+    page = open_homepage
+
+    # first duty
+    page.fill("#duty-number-input", "1")
+    page.fill("#duty-description-input", "First Duty")
+    page.click("text=Add Duty")
+
+    # dupicate duty number
+    page.fill("#duty-number-input", "1")
+    page.fill("#duty-description-input", "Duplicate Duty")
+    page.click("text=Add Duty")
+
+    expect(page.locator("#duties-list")).not_to_contain_text("1 - Duplicate Duty")
