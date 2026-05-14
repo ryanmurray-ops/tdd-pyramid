dcfbc816c13215c9755b9251b520c7ca7f3064b0
Ryan Murray <ryan.murray@madetech.com>
2026-05-14 15:25:40 +0100

    implement basic create_duty function with formatting

--- duties.py
+++ duties.py
@@ -1,11 +1,16 @@
-def is_duplicate_duty_number(number, duties):
-    for duty in duties:
-        if duty.startswith(f"{number} -"):
-            return True
-    return False
+def create_duty(number, description, duties):
+    return f"{number} - {description}"
 
 def is_valid_duty_number(number):
     return bool(number)
 
 def is_valid_duty_description(description):
     return bool(description)
+
+def is_duplicate_duty_number(number, duties):
+    for duty in duties:
+        if duty.startswith(f"{number} -"):
+            return True
+    return False
+
+
--- tests/unit/test_create_duty.py
+++ tests/unit/test_create_duty.py
@@ -1,3 +1,5 @@
+from duties import create_duty
+
 def test_create_duty_returns_fromatted_string():
     result = create_duty("1", "My First Duty", [])
 
