328b9fec00cd4b56c6d94b719d936b40768e746c
Ryan Murray <ryan.murray@madetech.com>
2026-05-14 16:38:44 +0100

    test(integration): user can create duty via home route

--- tests/integration/test_home_route.py
+++ tests/integration/test_home_route.py
@@ -8,3 +8,15 @@ def test_huser_can_create_duty_via_home_route():
     })
 
     assert response.status_code == 200
+
+def test_user_can_create_duty_via_home_route():
+    client = app.test_client()
+
+    response = client.post("/", data={
+        "number": "1",
+        "description": "My First Duty"
+    })
+
+    response_text = response.get_data(as_text=True)
+
+    assert "1 - My First Duty" in response_text
