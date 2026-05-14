041ff7ca77fa954a614365aa81d903c199d24486
Ryan Murray <ryan.murray@madetech.com>
2026-05-14 18:27:46 +0100

    test(integration): improve integration test using mock and flexible assertion for create_duty call

--- tests/integration/test_home_route.py
+++ tests/integration/test_home_route.py
@@ -1,5 +1,7 @@
 from app import app
-def test_huser_can_create_duty_via_home_route():
+from unittest.mock import ANY, patch
+
+def test_home_route_returns_200():
     client = app.test_client()
 
     response = client.post("/", data={
@@ -12,11 +14,21 @@ def test_huser_can_create_duty_via_home_route():
 def test_user_can_create_duty_via_home_route():
     client = app.test_client()
 
-    response = client.post("/", data={
-        "number": "1",
-        "description": "My First Duty"
-    })
+    with patch("app.create_duty") as mock_create_duty:
+        mock_create_duty.return_value = "1 - My First Duty"
 
-    response_text = response.get_data(as_text=True)
+        response = client.post("/", data={
+            "number": "1",
+            "description": "My First Duty"
+        })
 
-    assert "1 - My First Duty" in response_text
+        response_text = response.get_data(as_text=True)
+
+        assert response.status_code == 200
+        assert "1 - My First Duty" in response_text
+
+        mock_create_duty.assert_called_once_with(
+            "1",
+            "My First Duty",
+            ANY
+        )
