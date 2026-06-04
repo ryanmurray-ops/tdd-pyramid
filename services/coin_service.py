61f6dc179f72dfffcb1c3cd71912564b210dcea1
Ryan Murray <ryan.murray@madetech.com>
2026-06-04 16:22:12 +0100

    feat(green): prevent duplicate coin names in CoinService

--- services/coin_service.py
+++ services/coin_service.py
@@ -5,6 +5,10 @@ class CoinService:
         self.coins = []
 
     def create_coin(self, name):
+        for coin in self.coins:
+            if coin.name == name:
+                return None
+            
         coin = Coin(name)
         self.coins.append(coin)
         return coin
