88cc6076619531cf69128286903fa4255f0b0335
Ryan Murray <ryan.murray@madetech.com>
2026-06-04 14:55:13 +0100

    test(red): Coin should generate unique UUID

--- tests/unit/test_coin.py
+++ tests/unit/test_coin.py
@@ -4,3 +4,11 @@ def test_coin_can_be_created():
     coin = Coin(name="Automate")
 
     assert coin.name == "Automate"
+
+def test_coin_has_unique_id():
+    first_coin = Coin(name="Automate")
+    second_coin = Coin(name="Automate")
+
+    assert first_coin.id is not None
+    assert second_coin.id is not None
+    assert first_coin.id != second_coin.id
