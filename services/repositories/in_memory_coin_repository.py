6d021e3c10e1b174b0d8efe90027607e860c2161
Ryan Murray <ryan.murray@madetech.com>
2026-06-07 16:08:48 +0100

    feat(green): update coin completion status in-memory repository

--- services/repositories/in_memory_coin_repository.py
+++ services/repositories/in_memory_coin_repository.py
@@ -15,9 +15,18 @@ class InMemoryCoinRepository:
         
         return None
     
+    def update_coin(self, coin_id, is_complete):
+        for coin in self._coins:
+            if coin.id == coin_id:
+                coin.is_complete = is_complete
+                return coin
+            
+        return None
+    
     def delete_coin(self, coin_id):
         for coin in self._coins:
             if coin.id == coin_id:
                 self._coins.remove(coin)
                 return True
+        
         return False
