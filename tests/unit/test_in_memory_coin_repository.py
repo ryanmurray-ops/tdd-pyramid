e9a1aa3955996a37ba813d0e693afbcdc98aa533
Ryan Murray <ryan.murray@madetech.com>
2026-06-07 15:47:29 +0100

    test(red): repository stores coin and increaes collection size

--- tests/unit/test_in_memory_coin_repository.py
+++ tests/unit/test_in_memory_coin_repository.py
@@ -1,5 +1,12 @@
+from coin import Coin
 from services.repositories.in_memory_coin_repository import InMemoryCoinRepository
 
 def test_repository_starts_empty():
     repository = InMemoryCoinRepository()
     assert repository.get_all_coins() == []
+
+def test_repositoy_can_store_a_coin():
+    repository = InMemoryCoinRepository()
+    coin = Coin("Automate")
+    repository.create_coin(coin)
+    assert len(repository.get_all_coins()) == 1
