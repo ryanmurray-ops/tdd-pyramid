ecd36831741ec74faea42469187546fed7312dd7
Ryan Murray <ryan.murray@madetech.com>
2026-06-08 12:01:26 +0100

    feat: implement database repository get_coin_by_id and allow coin id to be set

--- coin.py
+++ coin.py
@@ -1,8 +1,8 @@
 import uuid
 
 class Coin:
-    def __init__(self, name):
-        self.id = str(uuid.uuid4())
+    def __init__(self, name, id=None):
+        self.id = id or str(uuid.uuid4())
         self.name = name
         self.is_complete = False
         self.duties = []
--- services/repositories/database_coin_repository.py
+++ services/repositories/database_coin_repository.py
@@ -1,5 +1,20 @@
+from coin import Coin
 from models.coin_model import CoinModel
 
 class DatabaseCoinRepository:
-    def create_coin(self, name):
-        return CoinModel.create(name=name)
+    def create_coin(self, coin):
+        return CoinModel.create(
+            id=coin.id,
+            name=coin.name
+        )
+    
+    def get_coin_by_id(self, coin_id):
+        coin_model = CoinModel.get_or_none(CoinModel.id == coin_id)
+
+        if not coin_model:
+            return None
+
+        return Coin(
+            name=coin_model.name,
+            id=str(coin_model.id)
+        )
