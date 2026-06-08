44309e6b0ce342f368778e6d4ff2e45a53067157
Ryan Murray <ryan.murray@madetech.com>
2026-06-08 20:13:06 +0100

    test(red): define api behaviour for creating duties on a coin

---
+++ tests/integration/test_coin_duties_api.py
@@ -0,0 +1,12 @@
+def test_can_create_duty_for_an_existing_coin(app, client):
+    coin = app.coin_service.create_coin("Automate")
+
+    response = client.post(
+        f"/coins/{coin.id}/duties",
+        json={
+            "number": "1",
+            "description": "My First Duty"
+        }
+    )
+    
+    assert response.status_code == 201
