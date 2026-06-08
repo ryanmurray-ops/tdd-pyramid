0f7b9128c703366100fd000445537f0fe01dbd3b
Ryan Murray <ryan.murray@madetech.com>
2026-06-08 09:55:03 +0100

    test(red): add database coin creation test

--- tests/unit/test_database_coin_repository.py
+++ tests/unit/test_database_coin_repository.py
@@ -1,5 +1,14 @@
+from models.coin_model import CoinModel
 from services.repositories.database_coin_repository import DatabaseCoinRepository
 
 def test_database_repository_can_be_created():
     repository = DatabaseCoinRepository()
     assert repository is not None
+
+def test_database_repository_can_store_a_coin():
+    repository = DatabaseCoinRepository()
+    repository.create_coin("Automate")
+    stored_coin = CoinModel.get(
+        CoinModel.name == "Automate"
+    )
+    assert stored_coin.name == "Automate"
