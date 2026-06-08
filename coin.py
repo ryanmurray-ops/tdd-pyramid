375869addaefce378a754b967b18c6ec851837f0
Ryan Murray <ryan.murray@madetech.com>
2026-06-08 14:22:40 +0100

    feat: implement update coin

--- coin.py
+++ coin.py
@@ -1,8 +1,8 @@
 import uuid
 
 class Coin:
-    def __init__(self, name, id=None):
+    def __init__(self, name, id=None, is_complete=False):
         self.id = id or str(uuid.uuid4())
         self.name = name
-        self.is_complete = False
+        self.is_complete = is_complete
         self.duties = []
--- services/repositories/database_coin_repository.py
+++ services/repositories/database_coin_repository.py
@@ -6,17 +6,21 @@ class DatabaseCoinRepository:
         coin_models = CoinModel.select()
         return [
             Coin(
-                id=coin.id,
-                name=coin.name
+                id=str(coin.id),
+                name=coin.name,
+                is_complete=coin.is_complete
             )
             for coin in coin_models
         ]
 
     def create_coin(self, coin):
-        return CoinModel.create(
+        CoinModel.create(
             id=coin.id,
-            name=coin.name
+            name=coin.name,
+            is_complete=coin.is_complete
         )
+
+        return coin
     
     def get_coin_by_id(self, coin_id):
         coin_model = CoinModel.get_or_none(CoinModel.id == coin_id)
@@ -25,6 +29,22 @@ class DatabaseCoinRepository:
             return None
 
         return Coin(
+            id=str(coin_model.id),
             name=coin_model.name,
-            id=str(coin_model.id)
+            is_complete=coin_model.is_complete
+        )
+
+    def update_coin(self, coin_id, is_complete):
+        coin_model = CoinModel.get_or_none(CoinModel.id == coin_id)
+
+        if not coin_model:
+            return None
+        
+        coin_model.is_complete = is_complete
+        coin_model.save()
+
+        return Coin(
+            id=str(coin_model.id),
+            name=coin_model.name,
+            is_complete=coin_model.is_complete
         )
