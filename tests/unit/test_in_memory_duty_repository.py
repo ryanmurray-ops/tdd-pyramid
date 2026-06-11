2991bb4d39bae237579d497274a933872b1e05bc
Ryan Murray <ryan.murray@madetech.com>
2026-06-11 12:35:58 +0100

    test(red): InMemoryDutyRepositiory can create a duty

--- tests/unit/test_in_memory_duty_repository.py
+++ tests/unit/test_in_memory_duty_repository.py
@@ -1,5 +1,21 @@
+from coin import Coin
 from services.repositories.in_memory_duty_repository import InMemoryDutyRepository
 
 def test_repository_starts_empty():
     repository = InMemoryDutyRepository()
     assert repository.get_all_duties() == []
+
+def test_repository_can_create_duty():
+    repository = InMemoryDutyRepository()
+    coin = Coin("Automate")
+    
+    repository.create_duty(
+        number="D1",
+        description="My First Duty"
+    )
+
+    duties = repository.get_all_duties()
+
+    assert len(duties) == 1
+    assert duties[0]["number"] == "D1"
+    assert duties[0]["description"] == "My First Duty"
