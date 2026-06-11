1896806591bf726b81a25db6ba7dbf173a21d90e
Ryan Murray <ryan.murray@madetech.com>
2026-06-11 16:45:39 +0100

    test(red): duty service can create a duty via repository

--- tests/unit/test_duty_service.py
+++ tests/unit/test_duty_service.py
@@ -1,6 +1,16 @@
+
 from services.duty_service import DutyService
+from services.repositories.in_memory_duty_repository import InMemoryDutyRepository
 
-def test_duty_service_initially_has_empty_duties_list():
-    service = DutyService()
 
-    assert service.duties == []
+def test_service_can_create_duty():
+    repository = InMemoryDutyRepository()
+    service = DutyService(repository)
+
+    duty = service.create_duty(
+        number="D1",
+        description="My First Duty"
+    )
+
+    assert duty["number"] == "D1"
+    assert duty["description"] == "My First Duty"
