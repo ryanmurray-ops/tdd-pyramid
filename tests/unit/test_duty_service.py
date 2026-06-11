22ba4ec4937b756eb05f903193842b814ed83096
Ryan Murray <ryan.murray@madetech.com>
2026-06-11 16:54:54 +0100

    test(red): DutyService can get duty  by id via repository

--- tests/unit/test_duty_service.py
+++ tests/unit/test_duty_service.py
@@ -7,19 +7,19 @@ def test_service_can_create_duty():
     repository = InMemoryDutyRepository()
     service = DutyService(repository)
 
-    duty = service.create_duty(
+    duty_created = service.create_duty(
         number="D1",
         description="My First Duty"
     )
 
-    assert duty["number"] == "D1"
-    assert duty["description"] == "My First Duty"
+    assert duty_created["number"] == "D1"
+    assert duty_created["description"] == "My First Duty"
 
 def test_service_can_get_all_duties():
     repository = InMemoryDutyRepository()
     service = DutyService(repository)
 
-    duty = service.create_duty(
+    duty_created = service.create_duty(
         number="D1",
         description="My First Duty"
     )
@@ -29,3 +29,18 @@ def test_service_can_get_all_duties():
     assert len(duties) == 1
     assert duties[0]["number"] == "D1"
     assert duties[0]["description"] == "My First Duty"
+
+def test_service_can_get_duty_by_id():
+    repository = InMemoryDutyRepository()
+    service = DutyService(repository)
+
+    duty_created = service.create_duty(
+        number="D1",
+        description="My First Duty"
+    )
+
+    result = service.get_duty_by_id(duty_created["id"])
+
+    assert result["id"] == duty_created["id"]
+    assert result["number"] == "D1"
+    assert result["description"] == "My First Duty"
