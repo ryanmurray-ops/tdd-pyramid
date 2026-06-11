74feab1af4cebf63a2b924deab510f5e1a7e8007
Ryan Murray <ryan.murray@madetech.com>
2026-06-11 19:04:23 +0100

    GEt /duties returns duties from DutyService

--- tests/integration/test_duties_api.py
+++ tests/integration/test_duties_api.py
@@ -8,3 +8,20 @@ def test_get_duties_returns_empty_list(client):
 
     assert response.status_code == 200
     assert response.json == []
+
+def test_get_duties_can_return_data_from_services(app, client):
+    app.duty_service.create_duty(
+        number="D1",
+        description="My First Duty"
+    )
+
+    response = client.get("/duties")
+
+    assert response.status_code == 200
+    assert response.json == [
+        {
+            "id": response.json[0]["id"],
+            "number": "D1",
+            "description": "My First Duty"           
+        }
+    ]
