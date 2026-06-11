e9ac899a3389b2817a4799905b63b2037826f38f
Ryan Murray <ryan.murray@madetech.com>
2026-06-11 16:50:07 +0100

    test(red): duty service can get all duties via repository

--- tests/unit/test_duty_service.py
+++ tests/unit/test_duty_service.py
@@ -14,3 +14,18 @@ def test_service_can_create_duty():
 
     assert duty["number"] == "D1"
     assert duty["description"] == "My First Duty"
+
+def test_service_can_get_all_duties():
+    repository = InMemoryDutyRepository()
+    service = DutyService(repository)
+
+    duty = service.create_duty(
+        number="D1",
+        description="My First Duty"
+    )
+
+    duties = service.get_all_duties()
+
+    assert len(duties) == 1
+    assert duties[0]["number"] == "D1"
+    assert duties[0]["description"] == "My First Duty"
