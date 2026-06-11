cad3156597aee23530360d1a89f0cf1044f37cb2
Ryan Murray <ryan.murray@madetech.com>
2026-06-11 12:36:24 +0100

    feat: implement create duty function in in-memory repository

--- services/repositories/in_memory_duty_repository.py
+++ services/repositories/in_memory_duty_repository.py
@@ -1,6 +1,19 @@
+import uuid
+
 class InMemoryDutyRepository:
     def __init__(self):
         self._duties = []
 
     def get_all_duties(self):
         return self._duties
+    
+    def create_duty(self, number, description):
+        duty = {
+            "id": str(uuid.uuid4()),
+            "number": number,
+            "description": description
+        }
+
+        self._duties.append(duty)
+        return duty
+    
