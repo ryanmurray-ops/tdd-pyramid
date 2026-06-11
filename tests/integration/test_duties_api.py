1774b090eb9cd2b7defc1d7e6738eed9adc98e50
Ryan Murray <ryan.murray@madetech.com>
2026-06-11 18:51:18 +0100

    test(green): confirm GET /duties returns a list of duties

--- tests/integration/test_duties_api.py
+++ tests/integration/test_duties_api.py
@@ -2,3 +2,9 @@ def test_get_duties_returns_200(client):
     response = client.get("/duties")
 
     assert response.status_code == 200
+
+def test_get_duties_returns_empty_list(client):
+    response = client.get("/duties")
+
+    assert response.status_code == 200
+    assert response.json == []
