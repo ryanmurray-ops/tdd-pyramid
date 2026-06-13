57b6258a294cd7c137c8513442ba61a086426634
Ryan Murray <ryan.murray@madetech.com>
2026-06-13 15:45:45 +0100

    test(red): get_all_duties returns an empty list

--- tests/unit/test_database_duty_repository.py
+++ tests/unit/test_database_duty_repository.py
@@ -4,3 +4,8 @@ from services.repositories.database_duty_repository import DatabaseDutyRepositor
 def test_database_duty_repository_can_be_created():
     repository = DatabaseDutyRepository()
     assert repository is not None
+
+def test_database_duty_repository_can_be_created():
+    repository = DatabaseDutyRepository()
+    duties = repository.get_all_duties()
+    assert duties == []
