29d18b9a53e3cd67a7ea2ce0f3d9df51ae64d06f
Ryan Murray <ryan.murray@madetech.com>
2026-06-13 16:19:10 +0100

    feat: implement create_duty function

--- services/repositories/database_duty_repository.py
+++ services/repositories/database_duty_repository.py
@@ -1,3 +1,18 @@
+from models.duty_model import DutyModel
+
 class DatabaseDutyRepository:
     def get_all_duties(self):
         return []
+    
+    def create_duty(slef, coin_id, number, description):
+        duty = DutyModel.create(
+            coin=coin_id,
+            number=number,
+            description=description
+        )
+
+        return {
+            "coin_id": coin_id,
+            "number": duty.number,
+            "description": duty.description
+        }
