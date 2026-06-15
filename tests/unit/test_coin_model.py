9e6db150f95326ea55b51060952cd1df55ae5b65
Ryan Murray <ryan.murray@madetech.com>
2026-06-15 17:48:21 +0100

    test: coin name must be unique

--- tests/unit/test_coin_model.py
+++ tests/unit/test_coin_model.py
@@ -1,4 +1,6 @@
 import uuid
+import pytest
+from peewee import IntegrityError
 
 from models.coin_model import CoinModel
 
@@ -10,4 +12,10 @@ def test_coin_has_uuid_coin_id():
     coin = CoinModel.create(name="Automate")
     assert coin.id is not None
     assert isinstance(coin.id, uuid.UUID)
+
+def test_coin_names_must_be_unique():
+    CoinModel.create(name="Automate")
+    with pytest.raises(IntegrityError):
+        CoinModel.create(name="Automate")
+
     
