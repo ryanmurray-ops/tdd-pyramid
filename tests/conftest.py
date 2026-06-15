1862d6a291a74ddf1542f2884039b39b0fa5a330
Ryan Murray <ryan.murray@madetech.com>
2026-06-15 18:13:11 +0100

    reset database schema at start of test session

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from app import app
+from database.db import db
 from models.coin_model import CoinModel
 
 @pytest.fixture(autouse=True)
@@ -10,6 +11,14 @@ def reset_duties():
 def open_homepage(page):
     page.goto("/")
     return page
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database():
+    db.connect(reuse_if_open=True)
+    db.drop_tables([CoinModel], safe=True)
+    db.create_tables([CoinModel])
+    yield
+    db.close()
 
 @pytest.fixture(autouse=True)
 def clean_db():
