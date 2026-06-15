8ae9ebad3f6be286d29a3e79007ada84aa9a7cc5
Ryan Murray <ryan.murray@madetech.com>
2026-06-15 17:27:42 +0100

    test: coin model assigns an id on creation

--- tests/unit/test_coin_model.py
+++ tests/unit/test_coin_model.py
@@ -1,5 +1,13 @@
+import uuid
+
 from models.coin_model import CoinModel
 
 def test_can_create_coin():
     coin = CoinModel.create(name="Automate")
     assert coin.name == "Automate"
+
+def test_coin_has_uuid_coin_id():
+    coin = CoinModel.create(name="Automate")
+    assert coin.id is not None
+    assert isinstance(coin.id, uuid.UUID)
+    
