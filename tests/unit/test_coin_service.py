c198c3e683893da199073f096ab8850772e891c1
Ryan Murray <ryan.murray@madetech.com>
2026-06-15 18:43:04 +0100

    test: retrieve all coins from service memory

--- tests/unit/test_coin_service.py
+++ tests/unit/test_coin_service.py
@@ -7,3 +7,14 @@ def test_can_create_coin():
 
     assert len(service.coins) == 1
     assert service.coins[0] == "Automate"
+
+def test_can_get_all_coins():
+    service = CoinService()
+    service.create_coin("Automate")
+    service.create_coin("Deploy")
+
+    coins = service.get_all_coins()
+
+    assert len(coins) == 2
+    assert coins[0] == "Automate"
+    assert coins[1] == "Deploy"
