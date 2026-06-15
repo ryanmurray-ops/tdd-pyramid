c403777de8793551087a8ca851d6d049d257d45e
Ryan Murray <ryan.murray@madetech.com>
2026-06-15 18:13:38 +0100

    feat: add is_completed field to CoinModel with default false

--- models/coin_model.py
+++ models/coin_model.py
@@ -1,10 +1,11 @@
 import uuid
-from peewee import Model, CharField, UUIDField
+from peewee import BooleanField, Model, CharField, UUIDField
 from database.db import db
 
 class CoinModel(db.Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4)
     name = CharField(unique=True)
+    is_complete = BooleanField(default=False)
 
     class Meta:
         database = db
