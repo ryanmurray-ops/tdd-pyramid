b63a4167f30aa6fc1621f0cd4c8852f88a327e23
Ryan Murray <ryan.murray@madetech.com>
2026-06-15 17:49:22 +0100

    feat: add unique check to UUID

--- models/coin_model.py
+++ models/coin_model.py
@@ -2,9 +2,9 @@ import uuid
 from peewee import Model, CharField, UUIDField
 from database.db import db
 
-class CoinModel(Model):
+class CoinModel(db.Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4)
-    name = CharField()
+    name = CharField(unique=True)
 
     class Meta:
         database = db
