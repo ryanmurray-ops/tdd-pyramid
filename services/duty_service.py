8b457c24961db15ba67a4625b0e0baabaff4567e
Ryan Murray <ryan.murray@madetech.com>
2026-06-16 14:52:44 +0100

    implement create_duty method able to store duty in database

--- services/duty_service.py
+++ services/duty_service.py
@@ -1,3 +1,12 @@
+from models.duty_model import DutyModel
+
 class DutyService:
     def __init__(self):
-        self.duties = []
+        self.duties = [] # LEGACY (Temporary Phase 1 compatability)
+
+    def create_duty(self, number, description):
+        duty = DutyModel.create(
+            number=number,
+            description=description
+        )
+        return duty
