ff5b3ae243d23f55db7a8dca4c2ccffa44f27ea1
Ryan Murray <ryan.murray@madetech.com>
2026-06-16 14:56:28 +0100

    test duty service can get all duties from database

--- tests/unit/test_duty_service.py
+++ tests/unit/test_duty_service.py
@@ -9,11 +9,25 @@ def test_duty_service_initially_has_empty_duties_list():
     assert service.duties == []
 # ==================
 
-def test_create_duty_saves_to_database():
+def test_service_can_create_duty_and_save_to_database():
     service = DutyService()
 
     duty = service.create_duty("D5", "CI/CD Pipeline")
 
     assert duty is not None
     assert DutyModel.get_or_none(DutyModel.number == "D5") is not None
+
+def test_serivice_can_get_all_duties():
+    service = DutyService()
+
+    service.create_duty("D5", "CI/CD Pipeline")
+    service.create_duty("D7", "Monitoring")
+
+    duties = service.get_all_duties()
+
+    duty_numbers = [duty.number for duty in duties]
+
+    assert len(duties) == 2
+    assert "D5" in duty_numbers
+    assert "D7" in duty_numbers
 
