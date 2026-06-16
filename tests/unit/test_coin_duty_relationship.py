d3a4a6291f3d312138f3355ce6dffb6e90837501
Ryan Murray <ryan.murray@madetech.com>
2026-06-16 13:02:21 +0100

    test: Coin-Duty relationship stores via db

--- tests/unit/test_coin_duty_relationship.py
+++ tests/unit/test_coin_duty_relationship.py
@@ -9,3 +9,13 @@ def test_coin_can_have_duties():
     coin.duties.add(duty)
 
     assert duty in coin.duties
+
+def test_coin_duty_relationship_stores_via_db():
+    coin = CoinModel.create(name="Automate")
+    duty = DutyModel.create(number="D5", description="CI/CD")
+
+    coin.duties.add(duty)
+
+    db_coin = CoinModel.get(CoinModel.id == coin.id)
+
+    assert duty in db_coin.duties
