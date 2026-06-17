f05bca422d4f0e74e7a38ca3788ceef97692ca43
Ryan Murray <ryan.murray@madetech.com>
2026-06-17 14:58:27 +0100

    test: coin controller can get all coins

--- tests/unit/test_coin_controller.py
+++ tests/unit/test_coin_controller.py
@@ -8,3 +8,13 @@ def test_controller_can_create_coin():
     assert created_coin["success"] is True
     assert created_coin["data"].name == "Automate"
     assert created_coin["error"] is None
+
+def test_controller_can_get_all_coins():
+    controller = CoinController()
+
+    controller.create_coin("Automate")
+    controller.create_coin("Deploy")
+
+    found_coins = controller.get_all_coins()
+
+    assert len(found_coins["data"]) == 2
