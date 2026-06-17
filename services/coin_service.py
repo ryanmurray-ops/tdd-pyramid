430c881e7afc52ec1c58eeeb39fa7e6af3f8439e
Ryan Murray <ryan.murray@madetech.com>
2026-06-17 11:24:00 +0100

    feat: get_coin_by_id returns error message when coin not found

--- services/coin_service.py
+++ services/coin_service.py
@@ -46,6 +46,13 @@ class CoinService:
     def get_coin_by_id(self, coin_id):
         coin = CoinModel.get_or_none(CoinModel.id == coin_id)
 
+        if not coin:
+            return {
+            "success": False,
+            "data": None,
+            "error": "Coin not found"
+        }
+
         return {
             "success": True,
             "data": coin,
